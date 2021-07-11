4ada72753ea167cd904b4fca6bdfa936994d277e
radha-intel <radha.patel@intel.com>
2021-07-11 22:56:59 +0530

    Numpy Intro

---
+++ Numpy/array.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a2 = np.empty((3,3))
+print("2D array", +a2)
+a3 = np.empty((3,3,3))
+print("3D array", +a3)
+min = a3.min()
+print("min is:", +min)
+max = a3.max()
+print("max is:", +max)
