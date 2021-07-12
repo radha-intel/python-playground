d5d8b4e84f7752ed78b35737ae2442c3800f65ab
radha-intel <radha.patel@intel.com>
2021-07-12 15:31:40 +0530

    Numpy Intro

--- Numpy/array.py
+++ Numpy/array.py
@@ -2,6 +2,8 @@ import numpy as np
 import csv
 import pandas as pd
 
+
+#Creating Numpy arrays with Random values
 a1 = np.random.rand(3,3)
 print(a1)
 a11 = np.random.rand(3,3,3)
@@ -10,16 +12,22 @@ a2 = np.empty((3,3))
 print("2D array", +a2)
 a3 = np.empty((3,3,3))
 print("3D array", +a3)
+
+#Finding minimum and maximum
 min = a3.min()
 print("min is:", +min)
 max = a3.max()
 print("max is:", +max)
+
+#Respacing the Numpy array
 reshaped = a3.reshape((-1,9))
 print("reshaped array", +reshaped)
 reshaped2 = a3.reshape((-1,3))
 print("reshaped array again", +reshaped2)
 restored = reshaped2.reshape(3,3,3)
 print("Restored 3D array", +restored)
+
+#reading .csv file in Numpy array
 data = pd.read_csv('data.csv').values
 #data = data.to_numpy()
 print("data from .csv file", +data)
