d3dd6b8d8b0ee9da7c4577449f4502cd3753146f
radha-intel <radha.patel@intel.com>
2021-07-14 09:08:11 +0530

    python

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def hello():
+    return render_template("hello.html")
+
+if __name__ == "__main__":
+    app.run()
