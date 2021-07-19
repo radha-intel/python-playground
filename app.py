9e817cd767cc67a4d0fcc2a1622264dc98a1b96c
radha-intel <radha.patel@intel.com>
2021-07-19 17:25:11 +0530

    Flask server

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from os import name
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/<name>')
-def hello(name):
-    return "<h1>Hello {}!</h1>".format(name)
+def user(name):
+    return f"<h1>Hello {name}!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
